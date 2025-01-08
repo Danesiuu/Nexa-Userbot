6ee846a3de0db5d46bd069b9dd0b1168c45de271
Danesiuu <danee12siuu@gmail.com>
2025-01-08 08:46:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@
 import os
 
 class Config(object):
-    APP_ID = os.environ.get("APP_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = os.environ.get("23990748", "")
+    API_HASH = os.environ.get("cf8f3b25ce525ebee03fdefb62facb4c", "")
     # Pyrogram Session
     PYRO_STR_SESSION = os.environ.get("PYRO_STR_SESSION", "")
     CMD_PREFIX = os.environ.get("CMD_PREFIX", ".")
